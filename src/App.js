0510345994f82c04c8d6e2bb6f83eb5bfabf0617
IshaqKHATTAK <mi5315562@gmail.com>
2023-06-13 17:46:20 -0700

    start of reac js project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
-
+function hel() {
+  alert("hellow you clicked")
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      <nav className="nav">
+        <a className="nav-link active" aria-current="page" href="#">Active</a>
+        <a className="nav-link" href="#">Link</a>
+        <a className="nav-link" href="#">Link</a>
+        <a className="nav-link disabled">Disabled</a>
+      </nav>
+      <div className="mb-3">
+        <label for="exampleFormControlTextarea1" className="form-label mt-3">Example textarea</label>
+        <textarea className="form-control border border-danger" id="exampleFormControlTextarea1" rows="3"></textarea>
+      </div>
+
+      
+      <div className="d-flex justify-content-center">
+        <button type="button" className="btn btn-dark w-50" onClick={hel()}>text details</button>
+      </div>
+      
+
+      
+    </>
   );
 }
 
